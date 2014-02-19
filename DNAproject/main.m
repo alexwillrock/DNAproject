06d5353c251ce16a6b7f7694eaa142f285c9dceb
Alex Willrock <willrock@211.ru>
2014-02-19 10:15:33 +0700

    init

---
+++ DNAproject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNAproject
+//
+//  Created by Willrock on 19.02.14.
+//  Copyright (c) 2014 Willrock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
